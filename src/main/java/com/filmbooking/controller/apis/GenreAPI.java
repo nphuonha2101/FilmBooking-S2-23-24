ea1bcd06077bd1e9628a2493c3d4c30b8f983d4c
Đoàn Quốc Đăng <21130014@st.hcmuaf.edu.vn>
2024-03-30 17:13:32 +0700

    -Update: Add GenreAPI

---
+++ src/main/java/com/filmbooking/controller/apis/GenreAPI.java
@@ -0,0 +1,52 @@
+package com.filmbooking.controller.apis;
+
+import com.filmbooking.hibernate.HibernateSessionProvider;
+import com.filmbooking.model.Genre;
+import com.filmbooking.model.Theater;
+import com.filmbooking.services.impls.GenreServicesImpl;
+import com.filmbooking.services.impls.TheaterServicesImpl;
+import com.filmbooking.utils.GSONUtils;
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(urlPatterns = {"/api/v1/genres/*", "/api/v1/genres"})
+public class GenreAPI extends HttpServlet {
+    private GenreServicesImpl genreServices;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HibernateSessionProvider sessionProvider = new HibernateSessionProvider();
+        genreServices = new GenreServicesImpl(sessionProvider);
+        Gson gson = GSONUtils.getGson();
+        String jsonResp = "";
+
+        String id = req.getParameter("genre-id");
+        if (id != null) {
+            Genre genre = genreServices.getByID(id);
+            jsonResp = gson.toJson(genre);
+        } else {
+            List<Genre> genreList = genreServices.getAll().getMultipleResults();
+
+            jsonResp = "[";
+
+            for (Genre genre : genreList) {
+                jsonResp += gson.toJson(genre);
+                if (genreList.indexOf(genre) != genreList.size() - 1) {
+                    jsonResp += ",";
+                }
+            }
+            jsonResp += "]";
+        }
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write(jsonResp);
+    }
+}
