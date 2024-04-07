803caaa47f17e7b3deba8e926d8426099925724e
Đoàn Quốc Đăng <21130014@st.hcmuaf.edu.vn>
2024-04-07 09:54:18 +0700

    -Update project: search optimization

--- src/main/webapp/resources/js/handlesSearch.js
+++ src/main/webapp/resources/js/handlesSearch.js
@@ -10,14 +10,16 @@ $(document).ready(function(){
         $('#result').html('');
         $('#state').val('');
         var searchField = $('#search-input').val();
-        var expression = new RegExp(searchField, "i");
-        $.getJSON('http://localhost:8080/api/v1/films', function(data) {
-            $.each(data, function(key, value){
-                if (value.filmName.search(expression) != -1)
-                {
-                    $('#result').append('<li class="list-group-item link-class"><img src="'+value.imgPath+'" height="40" width="40" class="img-thumbnail" /> '+value.filmName+'</li>');
-                }
+        if (searchField) {
+            var expression = new RegExp(searchField, "i");
+            $.getJSON('http://localhost:8080/api/v1/films', function(data) {
+                $.each(data, function(key, value){
+                    if (value.filmName.search(expression) != -1)
+                    {
+                        $('#result').append('<li class="list-group-item link-class"><a href="http://localhost:8080/film-info?film=' + value.slug + '"><img src="'+value.imgPath+'" height="40" width="40" class="img-thumbnail" /> '+value.filmName+'</a></li>');
+                    }
+                });
             });
-        });
+        }
     });
 });
