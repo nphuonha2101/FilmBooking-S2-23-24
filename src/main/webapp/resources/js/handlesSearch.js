4c1655cfc047c07ad0c65eba562b76ce9f25edfb
Đoàn Quốc Đăng <21130014@st.hcmuaf.edu.vn>
2024-05-12 20:59:59 +0700

    -- Update preject: + fix search with new api format -- issue: not show result(founded film but not show in result)

--- src/main/webapp/resources/js/handlesSearch.js
+++ src/main/webapp/resources/js/handlesSearch.js
@@ -1,32 +1,29 @@
-$(document).ready(function () {
-  $.ajaxSetup({ cache: false });
-  var throttledSearch = throttle(search, 1000);
-  $("#search-input").keyup(throttledSearch);
-});
-
 function search() {
   $("#result").html("");
   var searchField = $("#search-input").val();
   if (searchField.length >= 2) {
     var expression = new RegExp(searchField, "i");
-    $.getJSON("http://localhost:8080/api/v1/films", function (data) {
-      $.each(data, function (key, value) {
-        if (value.filmName.search(expression) != -1) {
-          $("#result").append(
-            '<a href="http://localhost:8080/film-info?film=' +
-              value.slug +
-              '" style="text-decoration: none"><li class="list-group-item link-class"><img src="' +
-              value.imgPath +
-              '" style="width: 60px; padding-right: 10px"/> ' +
-              value.filmName +
-              "<br> " +
-              value.director +
-              "<br> " +
-              value.cast +
-              "</li></a>"
-          );
-        }
-      });
+    $.getJSON("http://localhost:8080/api/v1/films?command=all", function (data) {
+      var listFilm = data.data;
+      console.log(listFilm);
+       for (let i = 0; i < listFilm.length; i++) {
+         if (listFilm[i].filmName.search(expression) != -1) {
+           console.log(listFilm[i].filmName)
+           $("#result").append(
+               '<a href="http://localhost:8080/film-info?film=' +
+               listFilm[i].slug +
+               '" style="text-decoration: none"><li class="list-group-item link-class"><img src="' +
+               listFilm[i].imgPath +
+               '" style="width: 60px; padding-right: 10px"/> ' +
+               listFilm[i].filmName +
+               "<br> " +
+               listFilm[i].director +
+               "<br> " +
+               listFilm[i].cast +
+               "</li></a>"
+           );
+         }
+       }
     });
   }
 }
@@ -42,3 +39,16 @@ function throttle(func, delay) {
     func(...args);
   };
 }
+
+
+$(document).ready(function () {
+  $.ajaxSetup({ cache: false });
+  var throttledSearch = throttle(search, 1000);
+  $("#search-input").keyup(throttledSearch);
+});
+
+
+
+
+
+
