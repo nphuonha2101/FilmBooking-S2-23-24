29d076f6276c1747c36d4754a5ae70dad42396eb
Đoàn Quốc Đăng <21130014@st.hcmuaf.edu.vn>
2024-05-18 09:27:36 +0700

    --update project: fixed bug(don't show result search film).

--- src/main/webapp/resources/js/handlesSearch.js
+++ src/main/webapp/resources/js/handlesSearch.js
@@ -1,31 +1,30 @@
 function search() {
-  $("#result").html("");
-  var searchField = $("#search-input").val();
-  if (searchField.length >= 2) {
-    var expression = new RegExp(searchField, "i");
-    $.getJSON("http://localhost:8080/api/v1/films?command=all", function (data) {
-      var listFilm = data.data;
-      console.log(listFilm);
-       for (let i = 0; i < listFilm.length; i++) {
-         if (listFilm[i].filmName.search(expression) != -1) {
-           console.log(listFilm[i].filmName)
-           $("#result").append(
-               '<a href="http://localhost:8080/film-info?film=' +
-               listFilm[i].slug +
-               '" style="text-decoration: none"><li class="list-group-item link-class"><img src="' +
-               listFilm[i].imgPath +
-               '" style="width: 60px; padding-right: 10px"/> ' +
-               listFilm[i].filmName +
-               "<br> " +
-               listFilm[i].director +
-               "<br> " +
-               listFilm[i].cast +
-               "</li></a>"
-           );
-         }
-       }
-    });
-  }
+    $("#search-result").css("display", "block");
+    $("#search-result").html("");
+    var searchField = $("#search-input").val();
+    if (searchField.length >= 2) {
+        var expression = new RegExp(searchField, "i");
+        $.getJSON("http://localhost:8080/api/v1/films?command=all", function (data) {
+            $.each(data.data, function (key, value) {
+                if (value.filmName.search(expression) != -1) {
+                    console.log(value.filmName);
+                    $("#search-result").append(
+                        '<a href="http://localhost:8080/film-info?film=' +
+                        value.slug +
+                        '" style="text-decoration: none"><li class="list-group-item link-class"><img src="' +
+                        value.imgPath +
+                        '" style="width: 60px; padding-right: 10px"/> ' +
+                        value.filmName +
+                        "<br> " +
+                        value.director +
+                        "</li></a>"
+                    );
+                }
+            });
+        });
+    }else{
+        $("#search-result").css("display", "none");
+    }
 }
 
 function throttle(func, delay) {
