14e3f5697108a260c7398c13a6fe90e9f82ff6ec
Nguyen Phuong Nha <nphuonha2101@gmail.com>
2024-05-25 14:09:52 +0700

    Update Project: fix: add Retention Runtime for @StringID and @IdAutoIncrement to make these appear at runtime

--- src/main/java/com/filmbooking/annotations/IdAutoIncrement.java
+++ src/main/java/com/filmbooking/annotations/IdAutoIncrement.java
@@ -1,5 +1,9 @@
 package com.filmbooking.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface IdAutoIncrement {
     boolean value() default true;
 }
