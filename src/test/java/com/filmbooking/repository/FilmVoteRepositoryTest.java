c5a35e52c42fe97b56d2073f451f887fcbf2ef46
Đoàn Quốc Đăng <21130014@st.hcmuaf.edu.vn>
2024-05-31 17:22:07 +0700

    feat: add FilmVoteRepository and FilmBookingRepository

---
+++ src/test/java/com/filmbooking/repository/FilmVoteRepositoryTest.java
@@ -0,0 +1,15 @@
+package com.filmbooking.repository;
+import com.filmbooking.model.FilmVote;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class FilmVoteRepositoryTest {
+
+                @Test
+                void select() {
+                    assertDoesNotThrow(() -> {
+                        FilmVoteRepository filmVoteRepository = new FilmVoteRepository(FilmVote.class);
+                        System.out.println(filmVoteRepository.selectAll());
+                    });
+                }
+}
