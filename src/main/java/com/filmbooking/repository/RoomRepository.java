27307c61e549c31c0a244fd1c1f8afba9b56215c
moclan01 <milepro98@gmail.com>
2024-05-31 21:11:38 +0700

    fix RoomMapper

---
+++ src/main/java/com/filmbooking/repository/RoomRepository.java
@@ -0,0 +1,31 @@
+package com.filmbooking.repository;
+
+import com.filmbooking.model.Room;
+import com.filmbooking.repository.mapper.RoomMapper;
+import org.jdbi.v3.core.mapper.RowMapper;
+
+import java.util.Map;
+
+public class RoomRepository extends AbstractRepository<Room>{
+    public RoomRepository(Class<Room> modelClass) {
+        super(modelClass);
+    }
+
+    @Override
+    RowMapper<Room> getRowMapper() {
+        return new RoomMapper();
+    }
+
+    @Override
+    public Map<String, Object> mapToRow(Room room) {
+        return Map.of(
+                "room_id", room.getRoomID(),
+                "room_name", room.getRoomName(),
+                "seat_rows", room.getSeatRows(),
+                "seat_cols", room.getSeatCols(),
+                "seat_data", room.getSeatData(),
+                "theater_id", room.getTheater().getTheaterID(),
+                "slug", room.getSlug()
+        );
+    }
+}
--- src/main/java/com/filmbooking/repository/mapper/RoomMapper.java
+++ src/main/java/com/filmbooking/repository/mapper/RoomMapper.java
@@ -3,6 +3,7 @@ package com.filmbooking.repository.mapper;
 import com.filmbooking.model.Room;
 import com.filmbooking.model.Theater;
 import com.filmbooking.repository.TheaterRepository;
+import com.filmbooking.utils.StringUtils;
 import org.jdbi.v3.core.config.ConfigRegistry;
 import org.jdbi.v3.core.mapper.RowMapper;
 import org.jdbi.v3.core.statement.StatementContext;
@@ -12,17 +13,16 @@ import java.sql.SQLException;
 
 public class RoomMapper implements RowMapper<Room> {
      TheaterRepository theaterRepository;
-     String[][] matrix;
     @Override
     public Room map(ResultSet rs, StatementContext ctx) throws SQLException {
         theaterRepository = new TheaterRepository(Theater.class);
-        matrix = new String[rs.getInt("seat_rows")][rs.getInt("seat_cols")];
+
         return new Room(
                 rs.getLong("room_id"),
                 rs.getString("room_name"),
                 rs.getInt("seat_rows"),
                 rs.getInt("seat_cols"),
-                matrix,
+                StringUtils.convertTo2DArr(rs.getInt("seat_rows") + " " + rs.getInt("seat_cols")),
                 rs.getString("seats_data"),
                 theaterRepository.select(rs.getLong("theater_id")),
                 null,
