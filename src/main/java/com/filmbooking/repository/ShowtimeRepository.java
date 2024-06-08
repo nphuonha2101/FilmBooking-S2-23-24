0b855cc282b80cd1ee9d201fc81d98cd7bae005b
Nguyen Phuong Nha <nphuonha2101@gmail.com>
2024-06-09 05:42:29 +0700

    Update Project: #57 - feat: add base method selectAllByFilmId and selectAllByRoomId for Showtime (not test) because project has some errors

--- src/main/java/com/filmbooking/repository/ShowtimeRepository.java
+++ src/main/java/com/filmbooking/repository/ShowtimeRepository.java
@@ -1,14 +1,51 @@
 package com.filmbooking.repository;
 
+import com.filmbooking.jdbi.connection.JdbiDBConnection;
 import com.filmbooking.model.Showtime;
 import com.filmbooking.repository.mapper.ShowtimeMapper;
+import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.mapper.RowMapper;
 
+import java.util.List;
 import java.util.Map;
 
 public class ShowtimeRepository extends AbstractRepository<Showtime> {
     public ShowtimeRepository(Class<Showtime> modelClass) {
         super(modelClass);
+    }
+
+    public List<Showtime> selectAllByFilmId(long filmId) {
+        try {
+            Handle handle = JdbiDBConnection.openHandle();
+            String sql = "SELECT * FROM showtimes WHERE film_id = :film_id";
+            System.out.println("Select all SQL: " + sql);
+            return handle.createQuery(sql)
+                    .bind("film_id", filmId)
+                    .map(getRowMapper())
+                    .list();
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+            return null;
+        } finally {
+            JdbiDBConnection.closeHandle();
+        }
+    }
+
+    public Showtime selectAllByRoomId(long roomID) {
+        try {
+            Handle handle = JdbiDBConnection.openHandle();
+            String sql = "SELECT * FROM showtimes WHERE room_id = :room_id";
+            System.out.println("Select all SQL: " + sql);
+            return handle.createQuery(sql)
+                    .bind("room_id", roomID)
+                    .map(getRowMapper())
+                    .one();
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+            return null;
+        } finally {
+            JdbiDBConnection.closeHandle();
+        }
     }
 
     @Override
