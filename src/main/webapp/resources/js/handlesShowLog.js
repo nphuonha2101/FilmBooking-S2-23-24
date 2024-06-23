c0f34a462acc37da5b2c4cf01cdc806163aa26ab
moclan01 <milepro98@gmail.com>
2024-06-23 22:15:49 +0700

    fix bugs not show logs

--- src/main/webapp/resources/js/handlesShowLog.js
+++ src/main/webapp/resources/js/handlesShowLog.js
@@ -11,7 +11,9 @@ $(function() {
             { data: 'action' },
             { data: 'level' },
             { data: 'targetTable' },
-            {data: '<button >Details</button>'}
+            {data: null,
+            defaultContent: '<button class="btn btn-danger details-btn">Details</button>',
+                orderable: false}
         ]
         // layout: {
         //     topStart: {
@@ -20,5 +22,28 @@ $(function() {
         //         ]
         //     }
         // }
+    });
+
+    $('#myTable tbody').on('click', '.details-btn', function() {
+        const data = table.row($(this).parents('tr')).data();
+        $.ajax({
+            url: `/api/v1/logs/${data.logID}`,
+            type: 'GET',
+            success: function(result) {
+                $('#logID').text(result.logID);
+                $('#username').text(result.user.username);
+                $('#action').text(result.action);
+                $('#level').text(result.level);
+                $('#targetTable').text(result.targetTable);
+                $('#beforeValue').text(result.beforeValue);
+                $('#afterValue').text(result.afterValue);
+                $('#createdAt').text(result.createdAt);
+                $('#updatedAt').text(result.updatedAt);
+                $('#detailsModal').modal('show');
+            },
+            error: function(xhr, status, error) {
+                alert('Error fetching log details: ' + xhr.responseText);
+            }
+        });
     });
 });
