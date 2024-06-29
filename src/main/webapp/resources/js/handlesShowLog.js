f73198a188ea52ad58af09f61edd7b1a72e2e931
moclan01 <milepro98@gmail.com>
2024-06-29 21:47:11 +0700

    fix logs

--- src/main/webapp/resources/js/handlesShowLog.js
+++ src/main/webapp/resources/js/handlesShowLog.js
@@ -1,42 +1,17 @@
 
 $(function() {
-    const table = $('#myTable').DataTable({
-
+    const table = $('#logTable').DataTable({
         ajax: {
             url: '/api/v1/logs?command=all',
             dataSrc: 'data'
         },
         columns: [
-
             { data: 'logID' },
-            { data: 'user.username',
-                defaultContent: 'Unknown User'},
+            { data: 'username', defaultContent: 'Unknown User' },
+            { data: 'reqIP' },
             { data: 'action' },
             { data: 'level' },
-            { data: 'targetTable' },
-            {data: null,
-            defaultContent: '<button class="btn btn-danger details-btn">Details</button>',
-                orderable: false}
+            { data: 'targetTable' }
         ]
-        // layout: {
-        //     topStart: {
-        //         buttons: [
-        //             'excel'
-        //         ]
-        //     }
-        // }
-    });
-
-    $('#myTable tbody').on('click', '.details-btn', function() {
-        const data = table.row($(this).parents('tr')).data();
-        $.ajax({
-            url: `/api/v1/logs/${data.logID}`,
-            type: 'GET',
-            success: function(result) {
-
-                $('#detailsModal').modal('show');
-            }
-        });
-
     });
 });
