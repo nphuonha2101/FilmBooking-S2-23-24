e807e0600641b2499118e832c2d53d0c14f3a39e
moclan01 <milepro98@gmail.com>
2024-07-01 21:15:21 +0700

    desgin detail log modal

--- src/main/webapp/resources/js/handlesShowLog.js
+++ src/main/webapp/resources/js/handlesShowLog.js
@@ -20,6 +20,7 @@ $(function() {
             }
         ]
     });
+
     $('#logTable tbody').on('click', '.details-btn', function() {
         const data = table.row($(this).parents('tr')).data();
 
@@ -30,24 +31,53 @@ $(function() {
                 // Xóa nội dung cũ của bảng
                 $('#modal-body-content').empty();
 
-                // Tạo một hàng mới với dữ liệu nhận được từ API
+                // Tạo các hàng mới với dữ liệu nhận được từ API
                 const logDetails = result.data;
                 selectLogId = logDetails.logID;
-                const row = `
+
+                const rows = `
                     <tr>
+                        <th>LogID</th>
                         <td>${logDetails.logID}</td>
+                    </tr>
+                    <tr>
+                        <th>Username</th>
                         <td>${logDetails.username}</td>
+                    </tr>
+                    <tr>
+                        <th>Request IP</th>
                         <td>${logDetails.reqIP}</td>
+                    </tr>
+                    <tr>
+                        <th>Action</th>
                         <td>${logDetails.action}</td>
+                    </tr>
+                    <tr>
+                        <th>Level</th>
                         <td>${logDetails.level}</td>
+                    </tr>
+                    <tr>
+                        <th>Target Table</th>
                         <td>${logDetails.targetTable}</td>
+                    </tr>
+                    <tr>
+                        <th>Before Value</th>
                         <td>${logDetails.beforeValue}</td>
+                    </tr>
+                    <tr>
+                        <th>After Value</th>
                         <td>${logDetails.afterValue}</td>
+                    </tr>
+                    <tr>
+                        <th>Created At</th>
                         <td>${logDetails.createdAt}</td>
+                    </tr>
+                    <tr>
+                        <th>Updated At</th>
                         <td>${logDetails.updatedAt}</td>
                     </tr>
                 `;
-                $('#modal-body-content').append(row);
+                $('#modal-body-content').append(rows);
 
                 // Hiển thị modal
                 $('#detailsModal').modal('show');
@@ -66,9 +96,28 @@ $(function() {
                 message: 'convert-level',
                 logID: selectLogId
             },
+            // success: function(response) {
+            //     console.log('Message sent successfully');
+            //     location.reload();
+            // },
             success: function(response) {
                 console.log('Message sent successfully');
-                location.reload();
+                $.ajax({
+                    url: `/api/v1/logs?command=id&id=${selectLogId}`,
+                    type: 'GET',
+                    success: function(result) {
+                        const logDetails = result.data;
+
+                        const row = table.row(function(idx, data, node) {
+                            return data.logID === selectLogId;
+                        });
+
+                        row.data(logDetails).draw();
+                    },
+                    error: function(xhr, status, error) {
+                        console.error('Error updating row: ' + error);
+                    }
+                });
             },
             error: function(xhr, status, error) {
                 console.error('Error sending message: ' + error);
