ada492a6ecca52c2f8f3891fd8bb4fe07306d0a7
moclan01 <milepro98@gmail.com>
2024-07-11 00:22:21 +0700

    send email if login fail 5 times

---
+++ src/main/java/com/filmbooking/email/SendFailLogin5TimesEmail.java
@@ -0,0 +1,25 @@
+package com.filmbooking.email;
+
+import com.filmbooking.enumsAndConstants.enums.LanguageEnum;
+import com.filmbooking.model.LogModel;
+
+import java.time.LocalDateTime;
+
+public class SendFailLogin5TimesEmail extends AbstractSendEmail{
+
+    public SendFailLogin5TimesEmail(){
+        super();
+    }
+    @Override
+    public AbstractSendEmail loadHTMLEmail(LanguageEnum language) {
+        String currentYear = String.valueOf(LocalDateTime.now().getYear());
+        this.loadHTMLEmail("failLogin5Times.html", language);
+        this.putEmailInfo("currentYear", currentYear);
+        return this;
+    }
+
+    @Override
+    public AbstractSendEmail loadLogData(LogModel logModel) {
+        return null;
+    }
+}
